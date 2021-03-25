df5decdcc3f0679b39431ac599d529fcb62d3d2c
Noah Holt <70286103+TidalTurtles@users.noreply.github.com>
2021-03-25 10:46:06 -0600

    Getting started
    created the file and added notes required by my teacher

---
+++ HoltNoahAssignment8.java
@@ -0,0 +1,20 @@
+
+public class HoltNoahAssignment8 {
+
+	public static void main(String[] args) {
+		/*Noah Holt
+		 * CS1150 Principles of computer science
+		 * m/w section
+		 * Due 31 mar
+		 * Assignment 8
+		 * This assignment is to create a grade organizer of sorts using hardcoded arrays
+		 * of grades and then from them, using multiple methods to get the highest grade
+		 * the average grade, and assigning letter grades to each array for the two given 
+		 * arrays and then printing them in a table for the user.
+		 * 
+		 * then some weird dog thing, still unsure about that part.
+		 */
+
+	}
+
+}
